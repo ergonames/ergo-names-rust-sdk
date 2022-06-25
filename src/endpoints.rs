6a6b7a6f3c226bbade6682e11a51a3c3c86dbad7
Zack Balbin <zack@zackbalbin.com>
2022-06-25 02:15:48 -0700

    url refactor

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -4,22 +4,14 @@ use serde_json::{Value};
 use crate::consts;
 
 pub fn get_token_data(token_name: &str, limit: u64, offset: u64) -> Result<Value> {
-    let mut url: String = consts::EXPLORER_API_URL.to_owned();
-    url.push_str("api/v1/tokens/search?query=");
-    url.push_str(token_name);
-    url.push_str("&limit=");
-    url.push_str(&limit.to_string());
-    url.push_str("&offset=");
-    url.push_str(&offset.to_string());
+    let url: String = format!("{}api/v1/tokens/search?query={}&limit={}&offset={}", consts::EXPLORER_API_URL, token_name, limit, offset);
     let resp: String = reqwest::blocking::get(url)?.text()?;
     let data: Value = serde_json::from_str(&resp)?;
     return Ok(data);
 }
 
 pub fn get_box_by_id(box_id: &str) -> Result<Value> {
-    let mut url: String = consts::EXPLORER_API_URL.to_owned();
-    url.push_str("api/v1/boxes/");
-    url.push_str(box_id);
+    let url: String = format!("{}api/v1/boxes/{}", consts::EXPLORER_API_URL, box_id);
     let resp: String = reqwest::blocking::get(url)?.text()?;
     let data: Value = serde_json::from_str(&resp)?;
     return Ok(data);
@@ -27,21 +19,14 @@ pub fn get_box_by_id(box_id: &str) -> Result<Value> {
 
 pub fn get_token_transaction_data(token_id: &str) -> Result<Value> {
     let total: u64 = crate::get_max_transactions_for_token(token_id);
-    let mut url: String = consts::EXPLORER_API_URL.to_owned();
-    url.push_str("api/v1/assets/search/byTokenId?query=");
-    url.push_str(token_id);
-    url.push_str("&offset=");
-    url.push_str(&(total-1).to_string());
+    let url: String = format!("{}api/v1/transactions/search?query={}&offset={}", consts::EXPLORER_API_URL, token_id, total-1);
     let resp: String = reqwest::blocking::get(url)?.text()?;
     let data: Value = serde_json::from_str(&resp)?;
     return Ok(data["items"].to_owned());
 }
 
 pub fn get_single_transactions_for_token(token_id: &str) -> Result<Value> {
-    let mut url: String = consts::EXPLORER_API_URL.to_owned();
-    url.push_str("api/v1/assets/search/byTokenId?query=");
-    url.push_str(token_id);
-    url.push_str("&limit=1");
+    let url: String = format!("{}api/v1/transactions/search?query={}&limit=1", consts::EXPLORER_API_URL, token_id);
     let resp: String = reqwest::blocking::get(url)?.text()?;
     let data: Value = serde_json::from_str(&resp)?;
     return Ok(data);
